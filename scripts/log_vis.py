4367730cc8bfe6391c1c162989fa6eb2d23f0dc4
swk <swkbupt@gmail.com>
2018-03-15 21:26:16 -0700

    added visdom support

---
+++ scripts/log_vis.py
@@ -0,0 +1,30 @@
+from visdom import Visdom
+import argparse
+import json
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--file')
+
+args = parser.parse_args()
+
+viz = Visdom()
+iter = []
+accuracy_cls = []
+loss_mask = []
+
+print(args.file)
+
+with open(args.file, 'r') as f:
+    for line in f:
+        if line.startswith("json_stats"):
+            stats = json.loads(line[12:])
+            iter.append(stats["iter"])
+            accuracy_cls.append(stats["accuracy_cls"])
+            loss_mask.append(stats["loss_mask"])
+
+iter_arr = np.array(iter)
+accuracy_cls_arr = np.array(accuracy_cls)
+loss_mask_arr = np.array(loss_mask)
+
+viz.line(X=iter_arr, Y=np.column_stack((accuracy_cls_arr, loss_mask_arr)))
